b982daff41833bb52656c4ea3e7b15d2e475895a
rodrigoaiz <rodrigoaiz@gmail.com>
2024-11-29 15:20:24 -0600

    Add Tailwind CSS configuration and styles, update HTML structure, and include footer and header components

---
+++ src/include/footer.php
@@ -0,0 +1,55 @@
+﻿        <footer class="course__footer">
+            <div class="container">
+                <div class="row align-items-center justify-content-between m-0 p-0">
+                    <div class="col-lg-4">
+                        <div class="row align-items-center justify-content-between">
+                            <div class="col-6">
+                                <img class="img-fluid" src="../../../assets/images/logo-pel.svg">
+                            </div>
+                            <div class="col-3">
+                                <img class="img-fluid" src="../../../assets/images/logo-cch.svg">
+                            </div>
+                            <div class="col-3">
+                                <img class="img-fluid" src="../../../assets/images/logo-unam.svg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <p class="course__footer--text">Universidad Nacional Autónoma de México. Ciudad Universitaria, Delegación Coyoacán, C. P. 04510, México, Ciudad de México.</p>
+                        <p class="course__footer--text">Este sitio web puede ser utilizado con fines no lucrativos siempre que se cite la fuente de conformidad con el Aviso Legal.</p>
+                    </div>
+                </div>
+            </div>
+            <hr class="w-100 my-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        © 2023 | Escuela Nacional Colegio de Ciencias y Humanidades | Portal Académico
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="row align-items-center justify-content-between">
+                            <div class="col-lg-7 d-flex flex-row justify-content-around">
+                                <a href="#">Inicio</a>
+                                <a href="#">Asignaturas</a>
+                                <a href="#">Acerca de</a>
+                            </div>
+                            <div class="col-lg-4 d-flex flex-row justify-content-around">
+                                <a href="">
+                                    <i class="ri-facebook-line"></i>
+                                </a>
+                                <a href="">
+                                    <i class="ri-youtube-fill"></i>
+                                </a>
+                                <a href="">
+                                    <i class="ri-instagram-line"></i>
+                                </a>
+                                <a href="">
+                                    <i class="ri-twitter-x-line"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
---
+++ src/include/header1.php
@@ -0,0 +1,23 @@
+<?php
+
+$ruta = dirname(__FILE__) . '/../../config.php';
+require_once($ruta);
+
+   require_login();
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <!--<meta http-equiv="Expires" content="0" />
+        <meta http-equiv="Pragma" content="no-cache" />-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fuentesss -->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+        <link rel="stylesheet" href="../../../assets/css/ali.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
