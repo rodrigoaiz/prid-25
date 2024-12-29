887e3eef3ef7861320ace430213eb0d3dc435346
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-12-29 17:37:45 -0600

    Update module 1 content and restructure menu links; add new references and ethical considerations for teaching

---
+++ src/modulo-1/6.php
@@ -0,0 +1,67 @@
+<?php
+include_once __DIR__ . '/../config.php';
+$currentPageId = 6;
+ob_start();
+?>
+<section>
+  <p>La Máxima Casa de Estudios cuenta con un marco normativo que rige sus tareas esenciales: docencia, investigación y difusión de la cultura. Evidentemente, la actividad docente en el CCH está regulada por dicho marco. Al respecto, existe una amplia variedad de documentos; a continuación, se enlistan algunos que se consideran básicos por su importancia para el ejercicio de la docencia:</p>
+  <ul class="ul-disc">
+    <li><strong>Ley Orgánica.</strong> Establece la función social y educativa de la UNAM, así como el fundamento de su estructura. Es la base en la cual descansa la organización de la Universidad.</li>
+    <li><strong>Estatuto General.</strong> Especifica la organización y estructura de la Universidad. Una sección a destacar es la de “Responsabilidades y sanciones”.</li>
+    <li><strong>Estatuto del Personal Académico.</strong> Indica la forma en que se organiza y reglamenta la situación del personal académico en la UNAM (técnicos y técnicas académicas , profesorado e investigadores).</li>
+    <li><strong>Marco Institucional de Docencia.</strong> Establece los principios generales de la docencia en la UNAM y los lineamientos generales acerca de los planes y programas de estudio.</li>
+    <li><strong>Código de ética de la UNAM.</strong> Documento publicado en julio de 2015 por el Consejo Universitario, el cual tiene como propósito regular la conducta de toda la comunidad universitaria (estudiantado , profesorado , trabajadores y trabajadoras). Está conformado por doce principios en los cuales se expresan los valores que deben orientar los fines de la Universidad (docencia, investigación y difusión de la cultura).</li>
+    <li><strong>Contrato colectivo de trabajo 2023-2025.</strong> Es el convenio que regula las relaciones laborales entre la Universidad y el sindicato de su personal académico (AAPAUNAM). Incluye regulaciones relativas a los salarios, días de descanso y vacaciones, prestaciones, licencias, incapacidades, entre otros.</li>
+    <li><strong>Herramientas para una docencia igualitaria, incluyente y libre de violencias.</strong> En esta guía, elaborada por la Coordinación para la Igualdad de Género (CIGU) de la UNAM, se especifican una serie de orientaciones sobre buenas prácticas docentes para construir ambientes educativos igualitarios y libres de violencia por razones de género.</li>
+    <li><strong>Cartilla universitaria de buenas prácticas enfocadas a poblaciones LGBTIQ+.</strong> Este documento también fue elaborado por la CIGU con la intención de difundir la existencia de identidades sexogenéricas disidentes y promover cambios culturales encaminados a lograr la igualdad y la inclusión en la UNAM.</li>
+  </ul>
+  <p class="font-bold text-lg my-10">Por su relevancia y pertinencia para la actividad docente, a continuación, revisaremos algunos artículos incluidos en los documentos previamente citados. </p>
+  <ul class="ul-disc mt-10">
+    <li><strong>Ley orgánica de la UNAM</strong> <em>Artículo 1o.-</em> La Universidad Nacional Autónoma de México es una corporación pública -organismo descentralizado del Estado- dotada de plena capacidad jurídica y que tiene por fines impartir educación superior para formar profesionistas, investigadores, profesores universitarios y técnicos útiles a la sociedad; organizar y realizar investigaciones principalmente acerca de las condiciones y problemas nacionales, y extender con la mayor amplitud posible los beneficios de la cultura. </li>
+    <li><a target="_blank" href="<?php echo PATH_DOCS . 'EstatutoGeneralUNAM.pdf'; ?>">Estatuto General de la UNAM</a> artículos 3º, 95, 96, 97 y 98.</li>
+    <li><a target="_blank" href="<?php echo PATH_DOCS . 'EstatutodelPersonalAcademico.pdf'; ?>">Estatuto del Personal Académico (EPA)</a> artículos 6, 55 y 56.</li>
+    <li><a target="_blank" href="<?php echo PATH_DOCS . 'MarcoInstitucionaldeDocencia.pdf'; ?>">Marco Institucional de Docencia.</a> principios 4, 7, 13, 14.</li>
+    <li><a target="_blank" href="<?php echo PATH_DOCS . 'codigo-etica-unam.pdf" '; ?>">Código de ética de la UNAM.</a> : documento completo.</li>
+    <li><strong>Contrato colectivo de trabajo 2023-2025.</strong> Es el convenio que regula las relaciones laborales entre la Universidad y el sindicato de su personal académico (AAPAUNAM). Incluye regulaciones relativas a los salarios, días de descanso y vacaciones, prestaciones, licencias, incapacidades, entre otros.</li>
+    <li><a target="_blank" href="<?php echo PATH_DOCS . 'CIGU-Herramientas-docentes.pdf'; ?>">Herramientas para una docencia igualitaria, incluyente y libre de violencias</a> apartados correspondientes a “1. Buenas prácticas docentes” y “2. Uso de lenguaje incluyente y no sexista”. </li>
+    <li><a target="_blank" href="<?php echo PATH_DOCS . 'cartillaLGBTIQ.pdf'; ?>">Cartilla universitaria de buenas prácticas enfocadas a poblaciones LGBTIQ+:</a> páginas 11 a 23, correspondientes a las “Buenas prácticas”.</li>
+  </ul>
+  <p>Conocer, reflexionar y actuar conforme a lo establecido en la normatividad es esencial para cualquier profesor. No obstante, es preciso reconocer que la aplicación de las normas y principios no es un proceso mecánico ni lineal, pues existen situaciones que requieren de un análisis y debates exhaustivos.</p>
+</section>
+<div class="grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-10 px-5 my-10">
+  <img class="mx-auto" src="<?php echo ASSET_URL ?>img/modulo-1/b1p06i01.webp" alt="cita 1">
+  <img class="mx-auto" src="<?php echo ASSET_URL ?>img/modulo-1/b1p06i02.webp" alt="cita 2">
+  <img class="mx-auto" src="<?php echo ASSET_URL ?>img/modulo-1/b1p06i03.webp" alt="cita 3">
+</div>
+<?php ob_start(); ?>
+<div class="grid  md:grid-cols-3 md:gap-10 items-center">
+  <p class="md:col-span-2 text-2xl"> Para saber más Revisa los siguientes textos que contienen información sobre la normatividad y la ética del quehacer docente:</p>
+  <div class="md:col-span-1">
+    <img class="mx-auto" src="<?php echo ASSET_URL ?>img/modulo-1/sabermas.webp" alt="Estudiantes CCH">
+  </div>
+</div>
+<ul class="ul-disc">
+  <li><a target="_blank" href="<?php echo PATH_DOCS . 'aapaunam-2023_2025.pdf'; ?>">Contrato colectivo de trabajo 2023-2025.</a>
+  </li>
+  <li><a href="http://abogadogeneral.unam.mx/PDFS/COMPENDIO/171.PDF'; ?>">Estatuto del Personal Académico de la UNAM (EPA).</a>
+  </li>
+  <li><a target="_blank" href="<?php echo PATH_DOCS . 'B1MarcoInstitucionalPSM2016.pdf'; ?>">Marco Institucional de Docencia en la UNAM.</a>
+  </li>
+  <li><a href="http://www.cch.unam.mx/consejo/" target="_blank">Micrositio del Consejo Técnico del CCH</a>, donde podrás encontrar los diferentes documentos que rigen la vida académica de nuestra institución.</li>
+  <li><a href="http://www.abogadogeneral.unam.mx/" target="_blank">Oficina de la Abogacía General de la UNAM.</a>
+  </li>
+  <li><a target="_blank" href="<?php echo PATH_DOCS . 'plagioyetica.pdf'; ?>">lagio y ética.</a></li>
+  <li><a target="_blank" href="<?php echo PATH_DOCS . 'igualdad-genero-unam-2013.pdf'; ?>">Lineamientos para la igualdad de género en la UNAM.</a> </li>
+  <li><a target="_blank" href="<?php echo PATH_DOCS . 'protocolo-atencion-integral-de-violencia-por-razones-de-genero-2022.pdf'; ?>">Protocolo para la atención de casos de violencia de género en la UNAM.</a></li>
+  <li><a href="https://coordinaciongenero.unam.mx/" target="_blank">Coordinación para la Igualdad de Género en la UNAM</a></li>
+  <li><a href="https://coordinaciongenero.unam.mx/2018/12/documento-fortalecimineto-politica-institucional-genero-unam/" target="_blank">Documento básico para el fortalecimiento institucional de género en la UNAM.</a></li>
+</ul>
+<?php
+$SaberContent = ob_get_clean();
+include BASE_PATH . '/include/ParaSaber.php';
+renderSaberContent($SaberContent);
+?>
+<?php
+$content = ob_get_clean();
+include BASE_PATH . '/include/templateContent.php';
+?>
---
+++ src/modulo-1/7.php
@@ -0,0 +1,49 @@
+<?php
+include_once __DIR__ . '/../config.php';
+$currentPageId = 7;
+ob_start();
+?>
+<section>
+  <ul class="ul-disc">
+    <li>(1979). <em>Documenta, No. 1</em>. Colegio de Ciencias y Humanidades. http://memoria.cch.unam.mx/index.php/revista/28</li>
+    <li>(2016). <em>45 años de la fundación del Colegio</em>. Eutopía. Revista del Colegio de Ciencias y Humanidades para el bachillerato. Número especial.</li>
+    <li>AAPAUNAM (2021). <em>Contrato colectivo de trabajo 2021-2023</em>. https://www.personal.unam.mx/Docs/Contratos/aapaunam-2021_2023.pdf</li>
+    <li>Bazán, J. (1988). <em>Un bachillerato de habilidades básicas</em>. Revista de la Educación Superior, 17 (65).CCH. (1996). <em>Plan de Estudios Actualizado</em>. México: Colegio de Ciencias y Humanidades-Unidad Académica del Ciclo de Bachillerato, UNAM.</li>
+    <li>CCH (2016). <em>Prontuario estadístico del Colegio de Ciencias y Humanidades</em>. http://www.cch.unam.mx/planeacion/sites/www.cch.unam.mx.planeacion/files/Agenda_2016_ok.pdf</li>
+    <li>CCH. (2014). <em>Micrositio del Consejo Técnico del CCH</em>. http://www.cch.unam.mx/consejo/</li>
+    <li>CEPAL (2010). <em>Panorama social de América Latina</em>. Santiago de Chile: Naciones Unidas-CEPAL.</li>
+    <li>Day, C. (2014). <em>Pasión por enseñar. La identidad personal y profesional del docente y sus valores</em>. Narcea. Recuperado de Proquest Ebook Central.</li>
+    <li>DGCCH/UNAM. (2012). <em>Comisiones Especiales para la Actualización de los Programas de Estudio de las Materias, DGCCH. Material de lectura</em>. México</li>
+    <li>Colegio de Ciencias y Humanidades. http://www.cch.unam.mx/sites/default/files/CEAPEM-sep2012.pdf</li>
+    <li>DGCCH. (2014). <em>Video 40 aniversario del Colegio de Ciencias y Humanidades</em>. México: DGCCH. http://www.cch.unam.mx/40aniversario</li>
+    <li>DGCCH. (2016). <em>Video 45 aniversario de la ENCCH [video]</em>. México: Teveunam. https://www.youtube.com/watch?v=llvE0Z1FdvE</li>
+    <li>DGIRE (2014) <em>¿Qué es una infografía? [video]</em>. YouTube https://www.youtube.com/watch?v=ERsVprw030k</li>
+    <li>Estadística UNAM (2024). <em>Series Estadísticas UNAM. Población escolar de bachillerato</em>. https://www.estadistica.unam.mx/series_inst/index.php</li>
+    <li>Gaceta CCH. (2001, 10 de octubre). <em>El Colegio de Ciencias y Humanidades: modelo y prácticas</em>. Gaceta CCH, Número Extraordinario 4.</li>
+    <li>Gaceta UNAM. (1971, 1 de febrero). <em>Se creó el Colegio de Ciencias y Humanidades</em>. Gaceta Amarilla. Gaceta UNAM, Número Extraordinario 2, 1-8.</li>
+    <li>García, T. (2004). <em>Aprendizajes Relevantes. La experiencia del Colegio en la modificación de sus programas de estudio</em>. Eutopía, (2), 7-17.</li>
+    <li>García, T. (2016) <em>Algunas acciones para mejorar la docencia y la formación de profesores en el CCH</em>. Nuevos cuadernos del Colegio, No. 8. http://memoria.cch.unam.mx/tmp/pdfarticulo/176/TRINIDAD_GARCIA_CAMACHO_NCC08_1457028869.pdf</li>
+    <li>González, J. y García, E. (coords). (2013). <em>Documentos y testimonios de la Historia del Colegio de Ciencias y Humanidades</em>. México: CCH-UNAM.</li>
+    <li>González, P. (1990). <em>Creación del Colegio de Ciencias y Humanidades</em>. En Nacimiento y Desarrollo del Colegio de Ciencias y Humanidades. México: CCH-UNAM.</li>
+    <li>INEE. (2011). <em>La educación media superior en México. Informe 2010-2011</em>. México: Instituto Nacional de Evaluación Educativa. 1ª edición. http://www.inee.edu.mx/index.php/acerca-del-inee/67-publicaciones/informes-anuales/26-la-educacion-media-superior-en-mexico</li>
+    <li>INEGI (2019). <em>Encuesta Nacional de Inserción Laboral de los Egresados de la Educación Media Superior (ENILEMS). Nota Técnica</em>. Recuperado de: https://www.inegi.org.mx/contenidos/programas/enilems/2019/doc/enilems_2019_nota_tecnica.pdf</li>
+    <li>Latapí, P. (1993). <em>Carta a un maestro</em>. En DIEZ para los Maestros (pp. 45-48). Sindicato Nacional de Trabajadores de la Educación. http://www.ieesa.org.mx/wp-content/uploads/2014/12/DIEZ-PARA-LOS-MAESTROS.pdf</li>
+    <li>Matus, L. (2013). <em>La construcción de una identidad docente, ¿un desafío para la política educativa?</em> Revista EXITUS, 3 (1), 75-87. http://www.ufopa.edu.br/portaldeperiodicos/index.php/revistaexitus/article/view/77/69</li>
+    <li>Netzahualcóyotl, A. (3 de julio del 2018). <em>Los hijos que no tendré</em>. [entrada de blog]. https://medium.com/@netza/los-hijos-que-no-tendr%C3%A9-8a546bae7bdf</li>
+    <li>Comisión de Trabajo Académico del Consejo Universitario (2003, 30 de octubre). <em>Marco Institucional de Docencia en la UNAM</em>. Gaceta UNAM, (3), p.22-24. http://www.nanolic.unam.mx/pagina/documentos/MARCO_INSTITUCIONAL.pdf</li>
+    <li>Oficina de la Abogacía general UNAM (2023). <em>Cartilla Universitaria de Buenas Prácticas Enfocadas a las Poblaciones LGBTIQ+</em>. México: UNAM. https://www.abogadogeneral.unam.mx/sites/default/files/archivos/Infografias2023/Junio/cartillaLGBTIQ.pdf</li>
+    <li>UNAM (2009). <em>Sitio de la oficina de la Abogacía General de la UNAM</em>. México: UNAM. http://www.abogadogeneral.unam.mx/</li>
+    <li>UNAM (2011). <em>Estatuto del Personal Académico de la UNAM (EPA)</em>. En Dirección General de Asuntos del Personal Académico (DGAPA). http://dgapa.unam.mx/html/normatividad/epa.html</li>
+    <li>UNAM (2013). <em>Lineamientos generales para la igualdad de género en la UNAM</em>. http://abogadogeneral.unam.mx/igualdad.pdf</li>
+    <li>UNAM (2014). <em>Estatuto General de la Universidad</em>. http://www.ddu.unam.mx/index.php/estatuto-general-de-la-unam</li>
+    <li>UNAM (2015). <em>Código de ética de la UNAM en Gaceta UNAM, 30 de julio de 2015</em>. http://dgapa.unam.mx/images/etica/2015_codigo-etica-unam.pdf</li>
+    <li>UNAM (2016). <em>Documento básico para el fortalecimiento de la política institucional de género de la UNAM</em>. https://consejo.unam.mx/comisiones/especial-de-equidad-de-genero/reglamentos-y-lineamientos/493-db</li>
+    <li>UNAM (2017). <em>Portal de Estadística Universitaria</em>. http://www.estadistica.unam.mx</li>
+    <li>UNAM (2019). <em>Protocolo para la atención de casos de violencia de género en la UNAM</em>. http://igualdaddegenero.unam.mx/wp-content/uploads/2019/03/Protocolo-2019.pdf</li>
+    <li>UNAM (2021) <em>Sitio de la Coordinación para la igualdad de género</em>. https://coordinaciongenero.unam.mx/quienes-somos/</li>
+  </ul>
+</section>
+<?php
+$content = ob_get_clean();
+include BASE_PATH . '/include/templateContent.php';
+?>
